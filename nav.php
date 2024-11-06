67aa86a09be67d663fbbf79ab6d48a9daa4a6ee3
Yuusouu <138218253+Yuusouu@users.noreply.github.com>
2024-11-06 12:54:41 +0800

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,8 @@
+<div id="nav">
+<ul>
+	<li><a href="register.php" title="Registration Page">Register</a></li>
+	<li><a href="register-view-users.php" title="Page 3">View Users</a></li>
+	<li><a href="page-4.php" title="Page 4">Pahina Quatro</a></li>
+	<li><a href="page-5.php" title="Page 5">Pahina Cinco</a></li>
+<ul>
+</div>
